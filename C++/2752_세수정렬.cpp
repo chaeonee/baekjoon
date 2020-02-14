9950eb5edad2d1cb72ff3e0acc79099f7682c5f8
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-02-14 16:25:32 +0900

    Update and rename 2752_세수정렬_방법2.cpp to 2752_세수정렬.cpp

--- C++/2752_세수정렬.cpp
+++ C++/2752_세수정렬.cpp
@@ -6,8 +6,8 @@ int main(){
 	cin >> arr[0] >> arr[1] >> arr[2];
 	
 	for(int i = 0; i < 3; i++){
-		for(int j = i+1; j < 3; j++){
-			if(arr[j] < arr[i]){
+		for(int j = i+1; j < 3; j++){ // 배열을 한 칸씩 이동하며 해당 배열의 값과 뒤쪽에 존재하는 수들을 비교
+			if(arr[j] < arr[i]){ // 오름차순 조건에 위배된다면(더 작은 수가 뒤에 있는 경우), 위치 변경
 				int tmp;
 				tmp = arr[i];
 				arr[i] = arr[j];
