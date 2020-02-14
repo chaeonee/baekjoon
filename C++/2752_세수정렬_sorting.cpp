0aa94279801ee1674cd6402413b6c741eaaa2b26
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-02-14 16:26:13 +0900

    Update 2752_세수정렬_sorting.cpp

--- C++/2752_세수정렬_sorting.cpp
+++ C++/2752_세수정렬_sorting.cpp
@@ -6,7 +6,7 @@ int main(){
 	int arr[3];
 	cin >> arr[0] >> arr[1] >> arr[2];
 	
-	sort(arr,arr+3);
+	sort(arr,arr+3); // sort함수를 이용하여 오름차순 정렬 진행
 	
 	cout << arr[0] << " " << arr[1] << " " << arr[2];
 	
