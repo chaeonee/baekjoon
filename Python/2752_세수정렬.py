1efc4c9df6d72afe50bd72f02fecb12dd1ad9749
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-02-14 16:08:29 +0900

    Update 2752_세수정렬.py

--- Python/2752_세수정렬.py
+++ Python/2752_세수정렬.py
@@ -2,9 +2,9 @@ if __name__ == '__main__':
 	num = list(map(int,input().split()))
 	
 	for i in range(3):
-		for j in range(i+1,3):
+		for j in range(i+1,3): # 현재 수와 뒤쪽에 존재하는 수들 비교
 			if num[i] > num[j]:
-				num[i], num[j] = num[j], num[i]
+				num[i], num[j] = num[j], num[i] # 큰 수가 더 앞에 있는 경우 자리 바꿈
 				
 	for n in num:
 		print(n, end=' ')
