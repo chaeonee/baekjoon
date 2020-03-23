e64cf615131ea52702addc9740666ea14809a0dc
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-03-23 15:45:05 +0900

    Create 9095_1,2,3더하기.py

---
+++ Python/9095_1,2,3더하기.py
@@ -0,0 +1,16 @@
+def getSum(n, cnt):
+	if n == 0: # n이 0이되면 1,2,3으로 이루어진 합 완성된 것이므로 cnt 1증가 후 return
+		return cnt+1
+		
+	for i in range(1,4):
+		if n-i >= 0: n-i가 0보다 작으면 모두 더했을 때 n보다 커지기 때문에 조건 추가
+			cnt = getSum(n-i,cnt)
+			
+	return cnt
+	
+if __name__ == '__main__':
+	T = int(input())
+	for _ in range(T):
+		n = int(input())
+		
+		print(getSum(n,0))
