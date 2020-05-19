21f83f44c96ad4861553ad67c3d063e9e44adeb5
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-05-19 22:55:56 +0900

    Update 13549_숨바꼭질3.py

--- Python/13549_숨바꼭질3.py
+++ Python/13549_숨바꼭질3.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
 		if 2*x <= 100000 and visit[2*x] == False: # 순간이동 먼저 넣어주어야 함 -> 시간이 덜 걸리기 때문
 			q.append([2*x,time])
 			visit[2*x] = True
-		if x-1 >= 0 and visit[x-1] == False: # 한 칸 앞으로 전진
+		if x-1 >= 0 and visit[x-1] == False: # 한 칸 뒤로 후진
 			q.append([x-1,time+1])
 			visit[x-1] = True
-		if x+1 <= 100000 and visit[x+1] == False: # 한 칸 뒤로 후진
+		if x+1 <= 100000 and visit[x+1] == False: # 한 칸 앞으로 전진
 			q.append([x+1,time+1])
 			visit[x+1] = True
 		
