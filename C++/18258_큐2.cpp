986bc5ebaa2be044786b92057911ca009e08df37
chaeonee <31956177+chaeonee@users.noreply.github.com>
2020-06-08 16:44:45 +0900

    Update 18258_큐2.cpp

--- C++/18258_큐2.cpp
+++ C++/18258_큐2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
-    cin.tie(0); // cin, cout 속도를 더 빠르게 해주기 위함
+    	cin.tie(0); // cin, cout 속도를 더 빠르게 해주기 위함
     
 	int N;
 	cin >> N;
