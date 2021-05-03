31f182bd4418dd64fd388945e1fec7d6f79c126e
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-05-03 15:34:15 +0900

    Create 15657_N과M(8).py

---
+++ Python/15657_N과M(8).py
@@ -0,0 +1,18 @@
+def getSeries(N, M, num, s, d, result):
+  if d == M:
+    for r in result:
+      print(r,end=' ')
+    print()
+    return
+  
+  for i in range(s,N):
+    result.append(num[i])
+    getSeries(N,M,num,i,d+1,result)
+    result.pop()
+
+if __name__ == '__main__':
+  N, M = list(map(int,input().split()))
+  num = list(map(int,input().split()))
+  num.sort()
+
+  getSeries(N,M,num,0,0,[])
