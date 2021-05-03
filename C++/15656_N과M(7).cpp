6234e47d9d33438073eff1ad81d4d30f86463bf0
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-05-03 15:35:25 +0900

    Create 15656_N과M(7).cpp

---
+++ C++/15656_N과M(7).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void getSeries(int *num, int N, int M, int d, vector<int> series){
+  if(d == M){
+    for(int i = 0; i < M; i++){
+      cout << series[i] << " ";
+    }
+    cout << '\n';
+    return;
+  }
+
+  for(int i = 0; i < N; i++){
+    series.push_back(num[i]);
+    getSeries(num,N,M,d+1,series);
+    series.pop_back();
+  }
+}
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+
+  int *num = new int[N];
+  for(int i = 0; i < N; i++){
+    cin >> num[i];
+  }
+  sort(num,num+N);
+
+  vector<int> series;
+  getSeries(num,N,M,0,series);
+}
