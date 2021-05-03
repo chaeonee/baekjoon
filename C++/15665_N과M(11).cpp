1cf4f780da68524bb4ec33fa3466fbc360870ec3
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-05-03 15:30:24 +0900

    Create 15665_N과M(11).cpp

---
+++ C++/15665_N과M(11).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void getSeries(int N, int M, vector<int> num, int d, vector<int> series){
+	if(d == M){
+		for(int i = 0; i < M; i++){
+			cout << series[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	
+	int pre_num = -1;
+	for(int i = 0; i < N; i++){
+		if(num[i] == pre_num){
+			continue;
+		}
+		pre_num = num[i];
+		
+		series.push_back(num[i]);
+		getSeries(N,M,num,d+1,series);
+		series.pop_back();
+	}
+}
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	
+	vector<int> num(N);
+	for(int i = 0; i < N; i++){
+		cin >> num[i];
+	}
+	sort(num.begin(),num.end());
+	
+	vector<int> series;
+	getSeries(N,M,num,0,series);
+	
+	return 0;
+}
