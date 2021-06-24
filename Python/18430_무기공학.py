c8ac9786b2e5f311ab3a486496b3a262bde0f226
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-06-24 17:58:34 +0900

    Update 18430_무기공학.py

--- Python/18430_무기공학.py
+++ Python/18430_무기공학.py
@@ -5,18 +5,17 @@ def makeBoomerang(s, N, M, material, visit, cur, max_strength):
 		max_strength = max(cur, max_strength)
 		return max_strength
 		
-	max_strength = makeBoomerang(s+1,N,M,material,visit,cur,max_strength)
+	max_strength = makeBoomerang(s+1,N,M,material,visit,cur,max_strength) # 현재칸을 중심으로 선택하지 않는 경우(case1)
 	
-	i  = s // M
-	j = s % M
+	i, j = s // M, s % M # 행, 열의 값을 칸의 번호르 체크해놓았기 때문에, 다시 행과 열의 값으로 바꿔주는 과정
 	
-	if visit[i][j]:
+	if visit[i][j]: # 이미 선택된 칸 (날개로)
 		return max_strength
 	
-	cur += 2*material[i][j]
-	visit[i][j] = True
+	cur += 2*material[i][j] # 현재칸이 중심으로 선택되었기 때문에 강도의 2배 더해 줌
+	visit[i][j] = True # 해당 칸 선택되었음을 표시
 	
-	if i+1 < N and j-1 >= 0 and not visit[i+1][j] and not visit[i][j-1]:
+	if i+1 < N and j-1 >= 0 and not visit[i+1][j] and not visit[i][j-1]: # 날개가 왼쪽 아래쪽(case2)
 		visit[i+1][j] = True
 		visit[i][j-1] = True
 		cur += material[i+1][j] + material[i][j-1]
@@ -25,7 +24,7 @@ def makeBoomerang(s, N, M, material, visit, cur, max_strength):
 		visit[i][j-1] = False
 		cur -= material[i+1][j] + material[i][j-1]
 		
-	if i-1 >= 0 and j-1 >= 0 and not visit[i-1][j] and not visit[i][j-1]:
+	if i-1 >= 0 and j-1 >= 0 and not visit[i-1][j] and not visit[i][j-1]: # 날개가 왼쪽 위쪽(case3)
 		visit[i-1][j] = True
 		visit[i][j-1] = True
 		cur += material[i-1][j] + material[i][j-1]
@@ -34,7 +33,7 @@ def makeBoomerang(s, N, M, material, visit, cur, max_strength):
 		visit[i][j-1] = False
 		cur -= material[i-1][j] + material[i][j-1]
 		
-	if i-1 >= 0 and j+1 < M and not visit[i-1][j] and not visit[i][j+1]:
+	if i-1 >= 0 and j+1 < M and not visit[i-1][j] and not visit[i][j+1]: # 날개가 오른쪽 위쪽(case4)
 		visit[i-1][j] = True
 		visit[i][j+1] = True
 		cur += material[i-1][j] + material[i][j+1]
@@ -43,7 +42,7 @@ def makeBoomerang(s, N, M, material, visit, cur, max_strength):
 		visit[i][j+1] = False
 		cur -= material[i-1][j] + material[i][j+1]
 		
-	if i+1 < N and j+1 < M and not visit[i+1][j] and not visit[i][j+1]:
+	if i+1 < N and j+1 < M and not visit[i+1][j] and not visit[i][j+1]: # 날개가 오른쪽 아래쪽(case5)
 		visit[i+1][j] = True
 		visit[i][j+1] = True
 		cur += material[i+1][j] + material[i][j+1]
@@ -51,9 +50,10 @@ def makeBoomerang(s, N, M, material, visit, cur, max_strength):
 		visit[i+1][j] = False
 		visit[i][j+1] = False
 		cur -= material[i+1][j] + material[i][j+1]
-		
-	cur -= 2*material[i][j]
-	visit[i][j] = False
+	
+	# 백트래킹	
+	cur -= 2*material[i][j] # 중심칸의 강도만큼 다시 빼줌
+	visit[i][j] = False # 방문 표시 제거
 			
 	return max_strength
 
