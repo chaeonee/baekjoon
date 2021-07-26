b543a1b4f19b2390a6d275ca7747f17cda4fc5e7
chaeonee <31956177+chaeonee@users.noreply.github.com>
2021-07-26 16:00:27 +0900

    Update 9465_스티커.py

--- Python/9465_스티커.py
+++ Python/9465_스티커.py
@@ -1,20 +1,23 @@
+import sys
+
 if __name__ == '__main__':
-	T = int(input())
+	T = int(sys.stdin.readline())
 	
 	for _ in range(T):
 		n = int(input())
 		sticker = []
-		for __ in range(2):
+		for _ in range(2):
 			sticker.append(list(map(int,input().split())))
 		
-          # 0은 현재 값으로 초기화, 1은 대각선 합 또는 현재 값으로 초기화
-		score = [[0]*n for __ in range(2)]
+		# 0은 현재 값으로 초기화, 1은 대각선 합 또는 현재 값으로 초기화
+		score = [[0]*n for _ in range(2)]
 		score[0][0] = sticker[0][0]
 		score[1][0] = sticker[1][0]
-		score[0][1] = max(sticker[1][0]+sticker[0][1],sticker[0][1])
-		score[1][1] = max(sticker[0][0]+sticker[1][1],sticker[1][1])
+		if n >= 2:
+			score[0][1] = max(sticker[1][0]+sticker[0][1],sticker[0][1])
+			score[1][1] = max(sticker[0][0]+sticker[1][1],sticker[1][1])
 		
-          # 대각선 한 칸 위, 대각선 두 칸 위 중 최댓값 더해주기 (나머지 가능한 이전 값들은 이미 계산되어진 것)
+		# 대각선 한 칸 위, 대각선 두 칸 위 중 최댓값 더해주기 (나머지 가능한 이전 값들은 이미 계산되어진 것)
 		for i in range(2,n):
 			score[0][i] = sticker[0][i] + max(score[1][i-1],score[1][i-2])
 			score[1][i] = sticker[1][i] + max(score[0][i-1],score[0][i-2])
